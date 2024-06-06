1534e5215786377d3d0c459ed458ee33baf347b3
Sanid-Assara <sanid.assara@gmail.com>
2024-06-06 12:07:55 +0200

    Add an if statement to specify the shift position if it is zero, negative, or positive.

--- script.js
+++ script.js
@@ -1,2 +1,40 @@
 const phrase = process.argv[2].toLowerCase();
 const shiftingPosition = Number(process.argv[3]);
+let cipherShifterAtZero = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+if (shiftingPosition > 0 && shiftingPosition <= 25) {
+  console.log(" up to 25");
+} else if (shiftingPosition < 0 && shiftingPosition >= -25) {
+  console.log(" down to -25");
+} else if (shiftingPosition === 0) {
+  console.log(" its a zero");
+}
+
+let cipherShifter = cipherShifterAtZero.slice(0, -2);
